257785da5e2ce365dff53f118f169ef3da59840d
Philipp Richter <phlpp.rchtr@protonmail.com>
2020-02-07 08:06:37 +0100

    new json to seach

--- podcast_search.py
+++ podcast_search.py
@@ -1,4 +1,11 @@
 import requests
+import json
 
 response = requests.get('https://itunes.apple.com/search?term=finanzen&entity=podcast&country=de')
 print(response.status_code)
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
